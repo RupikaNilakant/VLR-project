93e0f31406c849b943665a776cb1fce6637485c8
robohip <akalashi@andrew.cmu.edu>
2020-04-29 22:20:59 -0400

    train and data files

--- Meso/train.py
+++ Meso/train.py
@@ -7,12 +7,13 @@ import torch.nn.functional as F
 from torchvision import datasets, transforms
 import matplotlib.pyplot as plt
 from torch.utils.tensorboard import SummaryWriter
+from deepfake_data import deepfakeDataset
 
 #To DO: import model
 
 def validate(model, test_dataset, writer):
     model.eval()
-    for batch_idx, data in enumerate(test_dataset):
+    for batch_idx, data in enumerate(test_dataset_loader):
         image = data['image']
         ground_truth = data['ground_truth']
         prediction = model.(image)
@@ -29,8 +30,10 @@ def main():
     writer = SummaryWriter()
 
     #load dataset
-    train_dataset = torch.utils.data.DataLoader()
-    test_dataset = torch.utils.data.DataLoader()
+    train_dataset = deepfakeDataset
+    test_dataset = deepfakeDataset
+    train_dataset_loader = torch.utils.data.DataLoader()
+    test_dataset_loader = torch.utils.data.DataLoader()
 
     #define model
     #TO DO import model
@@ -41,28 +44,81 @@ def main():
     optimizer_adam = torch.optim.Adam(model.parameters(), lr=0.001)
     optimizer_SGD = torch.optim.SGD(model.parameters(), lr=0.001, momentum = 0.9)
     
-    #scheduler
+    #scheduler 
+    #step size relates to number of epoch
+    #in the paper they say they step every 1000 iterations
+    scheduler_adam = torch.optim.lr_scheduler.StepLR(optimizer_adam, step_size=5, gamma=0.1)
+    scheduler_SGD = torch.optim.lr_scheduler.StepLR(optimizer_SGD, step_size=5, gamma=0.1)
 
     #loss function
     loss_func_mse = nn.MSELoss()
 
     #train
-    for epoch in range(5):
-        for batch_idx, data in enumerate(train_dataset):
+    step = 0
+    for epoch in range(args.epochs):
+        num_batches = len(train_dataset_loader)
+        for batch_idx, data in enumerate(train_dataset_loader):
+            step = epoch*num_batches + batch_idx
+            #reset optimizer
+            optimizer_adam.zero_grad()
+            #set model to train
             model.train()
+            #get data from dataloader
             image = data['image']
             ground_truth = data['ground_truth']
+            #run through model and get prediction
             prediction = model(image)
 
+            #calculate loss
             loss = loss_func_mse(prediction,ground_truth)
+            #backprop
+            loss.backwards()
+            #step optimizer
+            optimizer_adam.step()
+
+
+            if step % args.log_every ==0:
+                print("Epoch: {}, Batch {}/{} has loss {}".format(epoch, batch_idx, num_batches, loss))
+
+        #step scheduler, steps based on set step size
+        scheduler_adam.setp()
 
 
 
 
 
+
+
+
+def parse_args():
+    """
+    :return:  args: experiment configs, device: use CUDA or cpu
+    """
+    parser = argparse.ArgumentParser(description='deepfake detection')
+    # config for dataset
+    parser.add_argument('--batch-size', type=int, default=1, metavar='N',
+                        help='input batch size for training (default: 64)')
+    parser.add_argument('--test-batch-size', type=int, default=1, metavar='N',
+                        help='input batch size for testing (default: 1000)')
+    parser.add_argument('--epochs', type=int, default=14, metavar='N',
+                        help='number of epochs to train (default: 14)')
+    parser.add_argument('--lr', type=float, default=0.001, metavar='LR',
+                        help='learning rate (default: 1.0)')
+    parser.add_argument('--no-cuda', action='store_true', default=False,
+                        help='disables CUDA training')
+    parser.add_argument('--log_every', type=int, default=100, metavar='N',
+                        help='how many batches to wait before logging training status')
+    parser.add_argument('--val_every', type=int, default=100, metavar='N',
+                        help='how many batches to wait before evaluating model')
+
+    parser.add_argument('--save-model', action='store_true', default=False,
+                        help='For Saving the current Model')
+    args = parser.parse_args()
+
+    return args
     
 
 
 if __name__ == '__main__':
-    args, device = parse_args()
+    args = parse_args()
     main()
